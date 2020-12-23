9cda18e205edf06ec8d6fcd765c2518cf10cfdb9
Aurangzaib <aurangzaib.danial@gmail.com>
2020-12-23 16:18:54 +0500

    released 0.2.2 for fixing bundler not being available in the runtime environment

--- lib/custom_jekyll.rb
+++ lib/custom_jekyll.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 require "custom_jekyll/version"
 require 'custom_jekyll/command_line_controller'
 require 'custom_jekyll/generator'
-
+require 'bundler'
 
 module CustomJekyll
   class Error < StandardError; end
   # Your code goes here...
 end
-
-
--- lib/custom_jekyll/version.rb
+++ lib/custom_jekyll/version.rb
@@ -1,3 +1,3 @@
 module CustomJekyll
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
 end
